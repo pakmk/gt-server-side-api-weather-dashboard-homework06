08cf5d5aa2088730c44fe7f19fe36f2185def5a0
min <pakmk8@gmail.com>
2020-06-17 20:34:23 -0400

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $("#new-search").on("click", function () {
+    event.preventDefault();
     var cityName = $("#search").val();
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -24,14 +25,59 @@ $(document).ready(function () {
       $("#today").append($("<h5>" + temp + " °F" + "</h5>"));
 
       var humidity = response.main.humidity;
+      //   console.log(humidity);
       $("#today").append($("<h5>" + humidity + "%" + "</h5>"));
 
       var windSpeed = response.wind.speed;
+      //   console.log(windSpeed);
       $("#today").append($("<h5>" + windSpeed + " MPH" + "</h5>"));
 
-      var currentDate = moment().subtract(10, "days").calendar();
-      console.log(currentDate);
-     
+      var weatherIcon = response.weather[0].icon;
+      // console.log(weatherIcon);
+      var icon = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
+      var img = $("<img>").attr("src", icon);
+      $("#today").append(img);
+
+      // var currentDate = moment().subtract(10, "days").calendar();
+      // console.log(currentDate);
+      // $("#today").append(currentDate);
+
+      var latitude = response.coord.lat;
+      // console.log(latitude);
+      var longitude = response.coord.lon;
+      // console.log(longitude);
+      var ultraURL =
+        "http://api.openweathermap.org/data/2.5/uvi?appid=f4bbf7147a555098803bbd3eb95abe19" +
+        "&lat=" +
+        latitude +
+        "&lon=" +
+        longitude;
+      // console.log(ultraURL)
+
+      $.ajax({
+        url: ultraURL,
+        method: "GET",
+      }).then(function (response) {
+        // console.log(response);
+        var uvData = response.value;
+        // console.log(uvData);
+        $("#today").append(uvData);
+      });
+    });
+
+    var weeklyURL =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&units=imperial&appid=f4bbf7147a555098803bbd3eb95abe19";
+
+    $.ajax({
+      url: weeklyURL,
+      method: "GET",
+    }).then(function (response) {
+      // console.log(response);
+      for (i = 0; i < response.list.length; i++);
+      // console.log(response.list);
+      
     });
   });
 });
